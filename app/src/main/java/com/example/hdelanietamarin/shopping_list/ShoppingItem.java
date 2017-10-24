a60248a82416c908cfd39d534945de6bb8cedf64
hugo <hugolonima@gmail.com>
2017-10-24 09:17:07 +0200

    Primer commit

---
+++ app/src/main/java/com/example/hdelanietamarin/shopping_list/ShoppingItem.java
@@ -0,0 +1,39 @@
+package com.example.hdelanietamarin.shopping_list;
+
+/**
+ * Created by h.de.la.nieta.marin on 23/10/2017.
+ */
+
+public class ShoppingItem {
+    private String text;
+    private boolean checked;
+
+    public ShoppingItem(String text) {
+        this.text = text;
+        this.checked=false;
+    }
+    public void toggleChecked(){
+        this.checked = !this.checked;
+    }
+
+    public ShoppingItem(String text, boolean checked) {
+        this.text = text;
+        this.checked = checked;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public void setChecked(boolean checked) {
+        this.checked = checked;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public boolean isChecked() {
+        return checked;
+    }
+}
